e519f8f69193489ced546d3f232791180ea92e6d
Potta Bathini Roja <PR00636417@techmahindra.com>
2020-08-12 18:01:52 +0530

    Updates

---
+++ precache-manifest.be2e0c21d784a218ec07faac562f0b01.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3ca143f3e33dc48d36bdb3d9f46083",
+    "url": "/shoppingrepo-app/index.html"
+  },
+  {
+    "revision": "568cf44c7001640ce5c6",
+    "url": "/shoppingrepo-app/static/css/main.39bf9bb5.chunk.css"
+  },
+  {
+    "revision": "fed82fe9570763fb5342",
+    "url": "/shoppingrepo-app/static/js/2.2b564805.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingrepo-app/static/js/2.2b564805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568cf44c7001640ce5c6",
+    "url": "/shoppingrepo-app/static/js/main.f3fa8625.chunk.js"
+  },
+  {
+    "revision": "4d8877648f58f5c087e9",
+    "url": "/shoppingrepo-app/static/js/runtime-main.48c60576.js"
+  },
+  {
+    "revision": "ee9e24747fe606c9e8e793f4a3ece141",
+    "url": "/shoppingrepo-app/static/media/FormalShirt.ee9e2474.jpeg"
+  },
+  {
+    "revision": "c19a8e148c4f9bee55b9aaa2e49b90c5",
+    "url": "/shoppingrepo-app/static/media/StripedSaree.c19a8e14.jpeg"
+  },
+  {
+    "revision": "5493a00057411b90ea28d4e61284fb29",
+    "url": "/shoppingrepo-app/static/media/babyShopcover.5493a000.jpg"
+  },
+  {
+    "revision": "93cc73b504724e529520bf1447f91f62",
+    "url": "/shoppingrepo-app/static/media/clothingcover.93cc73b5.jpg"
+  },
+  {
+    "revision": "f489c47eb210837343c15f7de9c33ec1",
+    "url": "/shoppingrepo-app/static/media/drumkeyboard.f489c47e.jpeg"
+  },
+  {
+    "revision": "ead2b4d7e5ca6e218223083fe73109be",
+    "url": "/shoppingrepo-app/static/media/dungaree.ead2b4d7.jpeg"
+  },
+  {
+    "revision": "46f97eecc66e7e5045c2e1698b263f0b",
+    "url": "/shoppingrepo-app/static/media/girlsparty.46f97eec.jpeg"
+  },
+  {
+    "revision": "7beb1f09de7650876e45f7aac247d38e",
+    "url": "/shoppingrepo-app/static/media/himalaya.7beb1f09.jpeg"
+  },
+  {
+    "revision": "84418d10b345aefb068cc553c71ebb67",
+    "url": "/shoppingrepo-app/static/media/hoodie.84418d10.jpeg"
+  },
+  {
+    "revision": "53cabec49f3ecea160d37b35029d0353",
+    "url": "/shoppingrepo-app/static/media/jacket.53cabec4.jpeg"
+  },
+  {
+    "revision": "c0e11bbfc641ed924d0689c3f65b74c3",
+    "url": "/shoppingrepo-app/static/media/junglebook.c0e11bbf.jpeg"
+  },
+  {
+    "revision": "da11487d4a899f629875a1d459f9c6c9",
+    "url": "/shoppingrepo-app/static/media/laughingboy.da11487d.jpeg"
+  },
+  {
+    "revision": "7083fffea290cca189e07ba80d869827",
+    "url": "/shoppingrepo-app/static/media/mamaearth.7083fffe.jpeg"
+  },
+  {
+    "revision": "a69128897546b6a8eb2905dc20257b22",
+    "url": "/shoppingrepo-app/static/media/pikachu.a6912889.jpeg"
+  },
+  {
+    "revision": "d78ea3783f8378e1ba3d375a46ddf02a",
+    "url": "/shoppingrepo-app/static/media/teddy.d78ea378.jpeg"
+  },
+  {
+    "revision": "a52040ffa4cad142efe7c75977c34c31",
+    "url": "/shoppingrepo-app/static/media/yellowDress.a52040ff.jpeg"
+  }
+]);
